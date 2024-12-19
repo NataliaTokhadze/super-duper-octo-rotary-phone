0b5a25b7c22d6687bf62e7085b157cc1dc70c152
natali <natalia.tokhadze@gmail.com>
2024-12-19 18:59:48 +0400

    nt

--- test-123-1/shop/admin.py
+++ test-123-1/shop/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from shop.models import Tag, Category, Item, Image
+from shop.filters import PriceFilter
 
 # class ItemLine(admin.TabularInline):
 #     model = Item
@@ -20,10 +21,17 @@ class ItemInLine(admin.TabularInline):
     extra = 1
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name']
+    list_display = ['id', 'name', 'first_five_items']
     search_fields = ['id', 'name']
     ordering = ['id']
     inlines = [ItemLine]
+    
+    def get_queryset(self, request):
+        existing_queryset = super().get_queryset(request)
+        return existing_queryset.prefetch_related('items')
+    
+    def first_five_items(self, obj):
+        return ", ".join(item.name for item in obj.items.all()[:5])
     
 class ItemAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'price']
@@ -31,6 +39,7 @@ class ItemAdmin(admin.ModelAdmin):
     ordering = ['price']
     fields = ['name', 'price', 'category', 'description']
     autocomplete_fields = ['category']
+    list_filter = [PriceFilter]
     inlines = [TagInLine]
     
 class TagAdmin(admin.ModelAdmin):
